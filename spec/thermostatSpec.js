c55c281729f359d4ea7c6dab8752a3a0fd8aa9e4
Daryl Watson <darylwtsn@gmail.com>
2015-05-27 10:46:39 +0100

    Cannot go lower than 10 degrees

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -5,7 +5,7 @@ describe("Thermostat", function () {
   });
 
   describe("Has a starting temperature", function () {
-    it("Has a starting temperature of 20 degrees", function() {
+    it("of 20 degrees", function() {
       expect(thermostat.temperature).toEqual(20);
     });
   });
@@ -31,6 +31,25 @@ describe("Thermostat", function () {
       thermostat.turnDown();
       thermostat.turnDown();
       expect(thermostat.temperature).toEqual(18)
+    });
+    it('by 10 degrees', function() {
+      for(i = 1; i < 11; i ++) {
+        thermostat.turnDown();
+      };
+      expect(thermostat.temperature).toEqual(10)
+    });
+    it('but cannot go lower than 10 degrees', function() {
+      for(i = 1; i < 12; i ++) {
+        thermostat.turnDown();
+      };
+      expect(thermostat.temperature).toEqual(10)
+    });
+  });
+
+  describe('Can reset the temperature', function() {
+    it('back to 20 degrees', function () {
+      thermostat.reset();
+      expect(thermostat.temperature).toEqual(20)
     });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -6,6 +6,12 @@ Thermostat.prototype.turnUp = function() {
   this.temperature ++ ;
 };
 
-Thermostat.prototype.turnDown =function() {
-  this.temperature -- ;
+Thermostat.prototype.turnDown = function() {
+  if(this.temperature > 10) {
+    this.temperature -- ;
+  };
+};
+
+Thermostat.prototype.reset = function() {
+  this.reset;
 };
